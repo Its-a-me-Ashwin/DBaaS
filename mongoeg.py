3e0f4a273c2be6134051d263f5d5d3010e0403c8
Ashwin R Bharadwaj <ashwinrb7799@gmail.com>
2020-05-14 11:40:44 +0530

    Not needed

--- mongoeg.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-userDB = mydb["rides"]
-DB = mydb["users"]
-usercol.drop()
-DB.drop()
-
